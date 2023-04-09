0efde04ed6283b0dde473adf59dbe4df5e2e9d8b
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2023-04-09 10:40:02 -0400

    found a bug with the rook

--- Pieces/Rook.py
+++ Pieces/Rook.py
@@ -8,17 +8,31 @@ class Rook(Piece):
 
     def getMoves(self, board):
         moves = []
-        for col in board[self.y]:
-            if board[col][self.y].isOccupied() != None:
+        for col in board:
+            if col == self.y:
+                continue 
+
+            if board[col][self.x].isOccupied() != None:
+                print(col)
+                print(self.x)
+                if board[col][self.x].isOccupied().getColor() != self.color:
+                    moves.append((self.x,col))
                 break
 
             moves.append((self.x, col))
 
         for row in ['a','b','c','d','e','f','g','h']:
+            if row == self.x:
+                continue 
+
             if board[self.y][row].isOccupied() != None:
+                if board[self.y][row].isOccupied().getColor() != self.color:
+                    moves.append((row,self.y))
                 break
 
             moves.append((row, self.y))
+
+        print(moves)
 
         return moves
 
