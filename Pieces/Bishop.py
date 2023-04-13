4b62c3db35bddb97a4dc986f14879c10a5bfd731
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2023-04-13 09:04:52 -0400

    push

--- Pieces/Bishop.py
+++ Pieces/Bishop.py
@@ -5,4 +5,9 @@ class Bishop(Piece):
     def __init__(self, x, y, color):
         Piece.__init__(self=self, name="bishop", x=x , y=y, color=color )
 
-    
+    def getMoves(self, board):
+        return 
+
+    def getUpRightMoves(self, board):
+        x = self.x
+        y = self.y
--- Pieces/Queen.py
+++ Pieces/Queen.py
@@ -1,6 +1,12 @@
+from Pieces.Bishop import Bishop
 from Pieces.Piece import Piece
+from Pieces.Rook import Rook
+from Pieces.Bishop import Bishop
 
 class Queen(Piece):
 
     def __init__(self, x, y, color):
         Piece.__init__(self=self, name="queen", x=x , y=y, color=color )
+
+    def getMoves(self, board):
+        return Rook.getMoves(board) + Bishop.getMoves(board)
