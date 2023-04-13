1d905899fccc8da3b2c406c6570050d0f336b100
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2023-04-13 16:25:22 -0400

    Got the kings piece to work

--- Pieces/King.py
+++ Pieces/King.py
@@ -5,3 +5,43 @@ class King(Piece):
 
     def __init__(self, x, y, color):
         Piece.__init__(self=self, name="king", x=x , y=y, color=color )
+
+    def getMoves(self, board):
+        moves=[]
+        
+        #all moves to the right
+        if chr(ord(self.x) + 1) != 'i':
+            x = chr(ord(self.x) + 1)
+            
+            if self.y - 1 > 1 and (board[self.y-1][x].isOccupied() == None or board[self.y-1][x].isOccupied().getColor() != self.color):
+                moves.append((x,self.y -1))
+
+            if self.y + 1 > 1 and (board[self.y+1][x].isOccupied() == None or board[self.y+1][x].isOccupied().getColor() != self.color):
+                moves.append((x,self.y + 1))
+
+            if (board[self.y][x].isOccupied() == None or board[self.y][x].isOccupied().getColor() != self.color):
+                moves.append((x,self.y))
+
+        #all moves to the left
+        if chr(ord(self.x) - 1) != chr(ord('a') - 1):
+            x = chr(ord(self.x) - 1)
+            
+            if self.y - 1 > 0 and (board[self.y-1][x].isOccupied() == None or board[self.y-1][x].isOccupied().getColor() != self.color):
+                moves.append((x,self.y -1))
+
+            if self.y + 1 < 9 and (board[self.y+1][x].isOccupied() == None or board[self.y+1][x].isOccupied().getColor() != self.color):
+                moves.append((x,self.y + 1))
+
+            if (board[self.y][x].isOccupied() == None or board[self.y][x].isOccupied().getColor() != self.color):
+                moves.append((x,self.y))
+
+        #move 1 up 
+        if self.y + 1 < 9 and (board[self.y+1][self.x].isOccupied() == None or board[self.y+1][self.x].isOccupied().getColor() != self.color):
+                moves.append((self.x,self.y + 1))
+
+        #move 1 down
+        if self.y - 1 > 0 and (board[self.y-1][self.x].isOccupied() == None or board[self.y-1][self.x].isOccupied().getColor() != self.color):
+                moves.append((self.x,self.y - 1))
+
+
+        return moves
